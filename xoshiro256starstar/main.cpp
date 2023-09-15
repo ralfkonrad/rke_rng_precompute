1d231257f4e0cd697ea4bd5963911f48e6a19985
Ralf Konrad <42419984+ralfkonrad@users.noreply.github.com>
2023-09-15 18:05:44 +0200

    Generating formatted xoshiro256** random numbers

--- xoshiro256starstar/main.cpp
+++ xoshiro256starstar/main.cpp
@@ -2,12 +2,35 @@
 // Created by ralf.eckel on 15.09.2023.
 //
 
-#include <fmt/core.h>
+#include <fmt/format.h>
+#include <vector>
 
 extern "C" {
 #include "xoshiro256starstar.c"
 };
 
+namespace rke {
+  void print(std::vector<uint64_t> randoms) {
+    fmt::print("{:>#18x}\n", fmt::join(randoms, ",\n"));
+  }
+}  // namespace rke
+
 int main() {
-  fmt::print("Hello world!");
+  s[0] = 0xcee90176f992409f;
+  s[1] = 0x27d84629aa1577c1;
+  s[2] = 0xbf02b000ab494475;
+  s[3] = 0xe5f75c3adab0eab6;
+
+  auto nrOfRandoms = 50;
+
+  std::vector<uint64_t> randoms;
+  randoms.reserve(nrOfRandoms);
+
+  for (auto i = 0; i < nrOfRandoms; ++i) {
+    randoms.push_back(next());
+  }
+
+  rke::print(randoms);
+
+  return 0;
 }
